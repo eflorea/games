819932e14549cab17048cb892ef0336fe60b6d25
Eduard Florea <edward.florea@gmail.com>
2020-04-07 20:30:25 -0700

    add layout concept and loading status

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+let callback = function(){
+    document.querySelector('body' ).classList.remove( 'loading' );
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    callback();
+} else {
+    document.addEventListener("DOMContentLoaded", callback);
+}
